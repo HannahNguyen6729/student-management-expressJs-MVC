91c74d2e8865bb70219957b5924fe75a8cb9fe0f
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2022-09-27 17:57:38 +0300

    add frontend part - layout

--- backend/app/server.js
+++ backend/app/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
 const router = require("./routers/rootRouter");
+app.use(cors());
 // modify req, res to JSON
 app.use(express.json());
 
---
+++ frontend/app/src/api/student.js
@@ -0,0 +1,58 @@
+// call api
+const getStudentList = async () => {
+  const res = await axios({
+    method: "GET",
+    url: "http://localhost:3000/students",
+    // url: `https://5fb26e6787ed490016ea8e2b.mockapi.io/students`,
+  });
+  return res.data;
+};
+
+const getStudentDetail = async (id) => {
+  const res = await axios({
+    method: "GET",
+    url: `http://localhost:3000/students/${id}`,
+    // url: `https://5fb26e6787ed490016ea8e2b.mockapi.io/students/${id}`,
+  });
+  return res.data;
+};
+
+const createStudent = async (student) => {
+  const res = await axios({
+    method: "POST",
+    url: `http://localhost:3000/students`,
+    // url: `https://5fb26e6787ed490016ea8e2b.mockapi.io/students`,
+    data: student,
+  });
+  console.log(res.data);
+  return res.data;
+};
+
+const updateStudent = async (id, student) => {
+  const res = await axios({
+    method: "PUT",
+    url: `http://localhost:3000/students/${id}`,
+    // url: `https://5fb26e6787ed490016ea8e2b.mockapi.io/students/${id}`,
+    data: student,
+  });
+  return res.data;
+};
+
+const deleteStudent = async (id) => {
+  const res = await axios({
+    method: "DELETE",
+    url: `http://localhost:3000/students/${id}`,
+    // url: `https://5fb26e6787ed490016ea8e2b.mockapi.io/students/${id}`,
+  });
+  return res.data;
+  //   $("#modalMessage").modal("show");
+  //   await getStudentList();
+};
+
+export {
+  getStudentList,
+  getStudentDetail,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+};
